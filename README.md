bfdb5e8ab8bd9d79783a0b4a29224205ff69e4dd
qiuzi <lihf@huidiancloud.com>
2021-03-04 21:53:17 +0800

    build: release 1.0.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# stylelint-config-qiuzi
+
+Default stylelint config used by qiuzi.
+
+## Installation
+
+```bash
+npm install stylelint-config-qiuzi --save-dev
+# Or with yarn:
+yarn add stylelint-config-qiuzi --dev
+```
+
+## Usage
+
+We provide a config for both CSS and SCSS. You can choose which one you would like to extend in your configuration:
+
+### CSS
+
+```json
+{
+  "extends": "stylelint-config-qiuzi/css"
+}
+```
+
+### SCSS
+
+```json
+{
+  "extends": "stylelint-config-qiuzi/scss"
+}
+```
---
+++ scss/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  'extends': [
+    'stylelint-config-recommended-scss',
+    '../css'
+  ],
+  'rules': {
+    'scss/at-function-named-arguments': 'never',
+    'scss/at-function-pattern': '^[a-z][a-z-]*$',
+    'scss/at-function-parentheses-space-before': 'never',
+    'scss/at-import-no-partial-leading-underscore': true,
+    'scss/at-import-partial-extension': 'never',
+    'scss/at-mixin-argumentless-call-parentheses': 'always',
+    'scss/at-mixin-parentheses-space-before': 'never',
+    'scss/at-rule-no-unknown': true,
+    'scss/dimension-no-non-numeric-values': true,
+    'scss/dollar-variable-colon-space-after': 'at-least-one-space',
+    'scss/dollar-variable-colon-space-before': 'never',
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/dollar-variable-pattern': '^[a-z][a-z-\\d]*$',
+    'scss/no-duplicate-mixins': true,
+    'scss/operator-no-newline-after': true,
+    'scss/operator-no-newline-before': true,
+    'scss/operator-no-unspaced': true,
+    'scss/selector-no-redundant-nesting-selector': true
+  }
+};
